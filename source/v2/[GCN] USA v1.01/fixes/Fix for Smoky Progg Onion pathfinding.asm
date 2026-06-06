5f44574b25541fd71686496ebdec86af2007af85
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2026-06-06 03:41:19 -0500

    Fix for Fix for Smoky Progg Onion Pathfinding
    Oops, I accidentally changed it so Smoky Progg always pursues the LAST active `GoalItem` in the `ItemMgr` rather than the NEAREST active `GoalItem` in the `ItemMgr`.  Not sure how I failed to consider this side-effect when modifying the condition.

--- source/v2/[GCN] USA v1.01/fixes/Fix for Smoky Progg Onion pathfinding.asm
+++ source/v2/[GCN] USA v1.01/fixes/Fix for Smoky Progg Onion pathfinding.asm
@@ -1,8 +1,12 @@
 #To be inserted at 800f2b94
 # r28 has GoalItem*
+# The task is to change `if (dist < minDist)` to `if (dist < minDist && stateID < GoalAI::GOAL_BootInit)``
 
+fcmpo	cr0, f1, f31      # Hijacked instruction, `if (dist < minDist)`
+bge-	END               # Short-circuit evaluation since both conditions use `bge-`
 lwz	r3, 0x02E4 (r28)  # r28->_2E4 is `AState<AICreature>* mCurrentState`
 lwz	r3, 0x0004 (r3)   # r28->_2E4->_04 is `AState<AICreature>::mStateId`
-cmpwi	r3, 5             # `if (stateID < GoalAI::GOAL_BootInit)`
+cmpwi	cr0, r3, 5        # `if (stateID < GoalAI::GOAL_BootInit)`
 
+END:
 # Following this Gecko Code, a `bge-` instruction is found.
--- source/v2/[NPC] USA/fixes/Fix for Smoky Progg Onion pathfinding.asm
+++ source/v2/[NPC] USA/fixes/Fix for Smoky Progg Onion pathfinding.asm
@@ -1,8 +1,12 @@
 #To be inserted at 8015c4f0
 # r31 has GoalItem*
+# The task is to change `if (dist < minDist)` to `if (dist < minDist && stateID < GoalAI::GOAL_BootInit)``
 
+fcmpo	cr0, f1, f31      # Hijacked instruction, `if (dist < minDist)`
+bge-	END               # Short-circuit evaluation since both conditions use `bge-`
 lwz	r3, 0x02EC (r31)  # r31->_2EC is `AState<AICreature>* mCurrentState`
 lwz	r3, 0x0004 (r3)   # r31->_2EC->_04 is `AState<AICreature>::mStateId`
-cmpwi	r3, 5             # `if (stateID < GoalAI::GOAL_BootInit)`
+cmpwi	cr0, r3, 5        # `if (stateID < GoalAI::GOAL_BootInit)`
 
+END:
 # Following this Gecko Code, a `bge-` instruction is found.
